2a77960b4ad18baa7e4dcad0dfc2a63784d3a8a5
mcorcias <mosescorcias@gmail.com>
2020-03-19 21:11:34 +0200

    DOM-ARRAY-METHODS

---
+++ DOM-ARRAY-METHODS/script.js
@@ -0,0 +1,79 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+//Add new obj to data arr
+const addData = obj => {
+  data.push(obj);
+  updateDOM();
+};
+
+//fetch random user and add money
+const getRandomUser = async () => {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+};
+
+//Update DOM
+function updateDOM(providedData = data) {
+  //Clear main dov
+  main.innerHTML = "<h2><strong>Person</strong>Wealth</h2>";
+
+  providedData.forEach(item => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong>
+     ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+}
+
+//Format number as money
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+function calculateWealth() {
+  const total = data.reduce((acc, user) => (acc += user.money), 0);
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>
+  ${formatMoney(total)}</strong></h3>`;
+  main.append(wealthEl);
+}
+
+function showMillionaires() {
+  data = data.filter(user => user.money >= 1000000);
+  updateDOM();
+}
+
+function sortByMoney() {
+  data.sort((a, b) => {
+    return b.money - a.money;
+  });
+  updateDOM();
+}
+
+function doubleMoney() {
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
+//Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByMoney);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
